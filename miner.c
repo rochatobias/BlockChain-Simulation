c107498f1955ff7e2a81d987346f0c10bc34c64f
NeruNeru367 <Nereruo@outlook.com>
2025-12-06 16:10:49 -0300

    Update miner.c / Calculo Hash e criações dos blocos

--- miner.c
+++ miner.c
@@ -3,6 +3,77 @@
 #include "mtwister.h"
 #include "miner.h"
 #include "structs.h"
+#include <string.h>
+#include <openssl/sha.h>
+
+#define SHA256_LEN 32
+
+void calcularHash(BlocoNaoMinerado *b, unsigned char hash[SHA256_LEN]){
+    
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+
+    SHA256_Update(&ctx, &b->numero, sizeof(b->numero));
+    SHA256_Update(&ctx, &b->nonce, sizeof(b->nonce));
+    SHA256_Update(&ctx, &b->data, sizeof(b->data));
+    SHA256_Update(&ctx, &b->hashAnterior, SHA256_LEN);
+
+    SHA256_Final(hash, &ctx);
+}
+
+void minerarBloco(BlocoNaoMinerado *b, unsigned char hash [SHA256_LEN]){
+    b->nonce = 0;
+
+    while(1){
+        calcularHash(b, hash);
+
+        if (hash[0] == 0){
+            break;
+        }
+
+        b->nonce++;
+    }
+}
+
+void atualizarHashAnt(BlocoNaoMinerado *prox, unsigned char hashAnterior[SHA256_LEN]){
+    memcpy(prox->hashAnterior, hashAnterior, SHA256_LEN);
+}
+
+BlocoMinerado criarBlocoGenesis(){
+    BlocoNaoMinerado bg;
+    memset(&bg, 0, sizeof(BlocoNaoMinerado)); //setando os 0s
+
+    bg.numero = 1; // numerando o  bloco
+
+    const char *msg = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+    memcpy(bg.data, msg, strlen(msg));
+
+    BlocoMinerado blocoFinal;
+    blocoFinal.bloco = bg;
+
+    minerarBloco(&blocoFinal.bloco, blocoFinal.hash);
+
+    return blocoFinal;
+}
+
+BlocoMinerado criarProxBloco(BlocoMinerado ant, unsigned int num){
+    BlocoNaoMinerado novo;
+    memset(&novo, 0, sizeof(novo));
+
+    novo.numero = num;
+
+    atualizarHashAnt(&novo, ant.hash);
+
+    const char *msg = "Transacoes do bloco";\
+    memcpy(novo.data, msg, strlen(msg));
+
+    BlocoMinerado final;
+    final.bloco = novo;
+
+    minerarBloco(&final.bloco, final.hash);
+
+    return final;
+}
 
 int main() {
     return 0;
