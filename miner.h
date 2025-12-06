2975de15ff64db44ef06f8984eaff18d3b516c99
NeruNeru367 <Nereruo@outlook.com>
2025-12-06 16:07:06 -0300

    Update miner.h prototipos

--- miner.h
+++ miner.h
@@ -6,6 +6,10 @@
 #include "structs.h"
 
 // Protótipos das funções
-
+void calcularHash(BlocoNaoMinerado *b, unsigned char hash[SHA256_LEN]);
+void minerarBloco(BlocoNaoMinerado *b, unsigned char hash [SHA256_LEN]);
+void atualizarHashAnt(BlocoNaoMinerado *prox, unsigned char hashAnterior[SHA256_LEN]);
+BlocoMinerado criarBlocoGenesis();
+BlocoMinerado criarProxBloco(BlocoMinerado ant, unsigned int num);
 
 #endif 
