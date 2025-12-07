e62a66a10f01a9c702c58cb2a80c6bb73f8071d6
Gabriel Henrique <ga.roldao@gmail.com>
2025-12-06 21:46:38 -0300

    Codigo final de transacoes com header corrigido

--- transactions.h
+++ transactions.h
@@ -1,11 +1,20 @@
 #ifndef TRANSACTIONS_H
 #define TRANSACTIONS_H
 
-// Bibliotecas 
-#include <stdio.h>
-#include "structs.h"
+#include "mtwister.h" 
 
-// Protótipos das funções
+// Protótipos das suas funções
 
+// Gera as transações aleatórias e preenche o vetor data
+int gerarDadosDoBloco(unsigned int numeroDoBloco, unsigned char dataBlock[], unsigned int carteiraOficial[], MTRand *r);
 
-#endif 
+// Atualiza o saldo da carteira (paga minerador e transfere valores)
+void atualizarCarteira(unsigned int numeroDoBloco, unsigned char dataBlock[], unsigned int carteiraOficial[], int qtdTransacoes);
+
+// Conta quantas transações existem num bloco (útil para leitura de disco do Tobias)
+int contarTransacoesNoBloco(unsigned char dataBlock[]);
+
+// Função auxiliar para imprimir o que tem no bloco (útil para menus)
+void imprimirBlocoDebug(unsigned char dataBlock[]);
+
+#endif
